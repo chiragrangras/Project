e9227aabea83408030bf78be946a1b226e6b1861
Chirag Rangras <chiragrangras012@gmail.com>
2024-04-14 17:06:07 +0530

    new file

---
+++ React_project_furniture_website/furniture_website/src/Stories.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Stories() {
+  return (
+    <div >
+      <h1>Happy Home Stories by Wakefit</h1>
+      <p>
+        Happy Home Stories by Wakefit is a collection of a few stories chosen
+        from over 4,60,000 verified reviews.<br/>
+        These are honest & unscripted conversations with real customers sharing their experiences with us.
+      </p>
+    </div>
+  );
+}
+export default Stories;
